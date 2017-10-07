9c11e72dc6bc1ac6bdca06912ffbbad44ec45d96
ZolotovAV <ZAV.pyc@gmail.com>
2017-10-07 18:15:30 +0400

    'bot_tel1'

--- misc.py
+++ misc.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-token = '325136693:AAGZElRqTDRGnL9bZAhcgfSmseqIRAFDOnA'
+#token = '325136693:AAGZElRqTDRGnL9bZAhcgfSmseqIRAFDOnA'
+token = '447719163:AAHVxJNqch-1azQ9hj9PurZKElxttW3nnZI'
